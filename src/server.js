ce7ae03cd6729d47bad7197ca6daf15ed6ee66af
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2022-05-12 13:52:33 +0900

    #1.8 Nicknames part Two
    - nickname을 입력하는 form 생성
    - 프론트에서 두타입으로 넘어온 JSON을 풀어서(JSON.Parse) 조건 처리
        - new_message인 경우
            - `${socket.nickname}: ${message.payload}` 형태로 send
        - nickname인 경우
            - `socket["nickname"] = message.payload;`

--- src/server.js
+++ src/server.js
@@ -20,10 +20,16 @@ const sockets = []; // fake database
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "Anonymous"; // 초기 닉네임 설정
   console.log("Connected to Browser ✔");
   socket.on("close", () => console.log("Disconnected from the Browser"));
-  socket.on("message", (message) => {
-    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    if(message.type === "new_message") { // 매세지 type인 경우
+      sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+    } else if(message.type === "nickname") { // 닉네임 type인 경우
+      socket["nickname"] = message.payload;
+    }
   });
 });
 
