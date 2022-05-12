04464588cb91cda2dc0f286270aa146143e487de
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2022-05-12 13:26:29 +0900

    #1.7 Nicknames part One (2022.05.12)
    - nickname을 입력하는 form 생성
    - 프론트에서 메세지를 두 타입으로 나눠 객체 → JSON문자 형태로 백엔드에 넘기기
        - 닉네임 : `{"type": "nickname", "payload": "input.value"}`
        - 메세지 : `{"type": "new_message", "payload": "input.value"}`

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,13 +1,21 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) { // 서버에 보내기 위해 메세지를 객체 ({type, payload}) 로 만들어 JSON 문자 형태로 변환
+  const msg = {type, payload};
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server ✔");
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("New message: ", message.data);
+  const li = document.createElement("li"); // li 생성
+  li.innerText = message.data; // li에 메세지 집어넣기
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
@@ -17,9 +25,17 @@ socket.addEventListener("close", () => {
 function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
   input.value = "";
 }
 
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
 
