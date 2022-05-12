401b1f1db737a8974948fcc21c7a3bfb7cb60a3e
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2022-05-12 09:52:24 +0900

    #1.4 WebSocket Messages
    프론트와 백엔드에서 메세지 주고받기

--- src/server.js
+++ src/server.js
@@ -16,10 +16,13 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new Websocket.Server({ server });
 
-function handleConnection(socket) {
-  console.log(socket);
-}
-
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✔");
+  socket.on("close", () => console.log("Disconnected from the Browser"));
+  socket.on("message", (message) => {
+    console.log(message.toString());
+  });
+  socket.send("hello!!");
+});
 
 server.listen(3000, handleListen);
