ca038d9ac8d5ebe51ab04fd93c58775ad7624295
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2022-05-12 15:57:02 +0900

    #2.1 Installing SocketIO
    - npm i socket.io 명령어로 socket.io 설치
    - 백엔드 ↔ 프론트엔드 socket.io 연결

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
+import SocketIo from "socket.io";
 import express from "express";
-import Websocket from "ws";
+// import Websocket from "ws";
 
 const app = express();
 
@@ -10,27 +11,32 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
+const httpServer = http.createServer(app);
+const wsServer = SocketIo(httpServer);
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
-const server = http.createServer(app);
-const wss = new Websocket.Server({ server });
-
-const sockets = []; // fake database
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anonymous"; // 초기 닉네임 설정
-  console.log("Connected to Browser ✔");
-  socket.on("close", () => console.log("Disconnected from the Browser"));
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    if(message.type === "new_message") { // 매세지 type인 경우
-      sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`)); // 자신과 다른 브라우저에 전송
-    } else if(message.type === "nickname") { // 닉네임 type인 경우
-      socket["nickname"] = message.payload;
-    }
-  });
+wsServer.on("connection", socket => {
+  console.log(socket);
 });
 
-server.listen(3000, handleListen);
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
+
+// const wss = new Websocket.Server({ server });
+
+// const sockets = []; // fake database
+
+// wss.on("connection", (socket) => {
+  //   sockets.push(socket);
+  //   socket["nickname"] = "Anonymous"; // 초기 닉네임 설정
+  //   console.log("Connected to Browser ✔");
+  //   socket.on("close", () => console.log("Disconnected from the Browser"));
+  //   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg);
+//     if(message.type === "new_message") { // 매세지 type인 경우
+//       sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`)); // 자신과 다른 브라우저에 전송
+//     } else if(message.type === "nickname") { // 닉네임 type인 경우
+//       socket["nickname"] = message.payload;
+//     }
+//   });
+// });
